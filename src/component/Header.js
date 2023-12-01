a334a129e5f537b99384624d6ec379cc7988a422
hong-311 <136770134+hong-311@users.noreply.github.com>
2023-12-01 17:10:29 +0900

    styled components 추가, Link태그 생성
    styled components 추가, Link태그 생성하였습니다

--- src/component/Header.js
+++ src/component/Header.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
 
-function Header(props) {
+const HeaderBlock = styled.div`
+    margin: 15px;
+    font-size: 15px;
+`;
+
+function Header() {
     return (
-        <div>
-            <h1>Beauty Boutique</h1>
-        </div>
+    <HeaderBlock>
+        <Link to='/'><h1>B.Boutique</h1></Link>
+    </HeaderBlock>
     );
 }
 
