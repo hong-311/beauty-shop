a563466f38c572220044c06c3c136be3dfdb17cb
hong-311 <136770134+hong-311@users.noreply.github.com>
2023-12-01 17:19:02 +0900

    styled components 추가
    styled components 추가하였습니다

--- src/component/Title.js
+++ src/component/Title.js
@@ -1,11 +1,19 @@
 import React from 'react';
-import '../scss/title.scss'
+import styled from "styled-components";
+
+const TitleBlock = styled.h1`
+  height: 70px;
+  font-size: 20px;
+  line-height: 70px;
+  text-align: center;
+  margin-bottom: 40px;
+`;
 
 function Title({name}) {
   return (
-    <div>
+    <TitleBlock>
       <h2>{name}</h2>
-    </div>
+    </TitleBlock>
   );
 }
 
