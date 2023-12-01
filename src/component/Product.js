d538718d84c86632f24f8afc4c34b8ddfa13cc8f
hong-311 <136770134+hong-311@users.noreply.github.com>
2023-12-01 17:17:58 +0900

    styled components추가, useParams hook 추가, find함수 사용
    styled components를 추가하고 useParams hook, find함수 사용하여 해당하는 정보를 찾아와 화면에 구현하였습니다

--- src/component/Product.js
+++ src/component/Product.js
@@ -1,20 +1,49 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
+import { initialBests } from './BestData';
+import styled from 'styled-components';
 
-// Product 컴포넌트
-function Product(props) {
-    const { name, image, descript, price } = props;
+const ProductBlock = styled.div`
+padding: 16px;
+margin: 16px;
+text-align: center;
 
-    return (
-        <div>
-            <h2>{name}</h2>
-            <img src={image} alt='상품이미지' />
-            <div>
-                <p>{descript}</p>
-                <p>\ {price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</p>
-            </div>
-        </div>
-    );
+h2 {
+  color: #333;
 }
 
+img {
+  max-width: 100%;
+  width: 460px;
+  height: 460px;
+  margin-top: 12px;
+}
+
+div {
+  margin-top: 12px;
+
+  p {
+    margin-bottom: 8px;
+  }
+}
+`;
+
+function Product() {
+  const { id } = useParams();
+  const selectedProduct = initialBests.find(product => product.id === parseInt(id));
+
+  const { name, image, descript, price } = selectedProduct;
+
+  return (
+    <ProductBlock>
+      <h2>{name}</h2>
+      <img src={image} alt="상품이미지" />
+      <div>
+        {descript && <p>{descript}</p>}
+        <p>￦ {price.toLocaleString()}</p>
+      </div>
+    </ProductBlock>
+  );
+}
 
 export default Product;
