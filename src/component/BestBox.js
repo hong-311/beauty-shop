6b64f3be8e92dc46dfafddf86cfb2d763c0a13e6
hong-311 <136770134+hong-311@users.noreply.github.com>
2023-12-01 17:04:09 +0900

    스타일 추가
    스타일 추가했습니다

--- src/component/BestBox.js
+++ src/component/BestBox.js
@@ -1,9 +1,18 @@
 import React from 'react';
-import '../scss/bestbox.scss';
+import styled from "styled-components";
+
+//스타일컴포넌트생성
+const BestBoxBlock = styled.div`
+  width: 1180px;
+  margin: 0 auto;
+  padding: 10px 10px 90px;
+  box-sizing: border-box;
+
+`;
 
 //모든 요소를 감싸고 가운데 영역으로 모아 줄 컴포넌트
 function BestBox({children}) {
-    return <div className="bestbox">{children}</div>;
+    return <BestBoxBlock className="bestbox">{children}</BestBoxBlock>;
 }
 
 export default BestBox;
