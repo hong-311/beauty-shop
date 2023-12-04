b35193f7def2ba3f1d1faf3b5289140ebd75b4ae
hong-311 <136770134+hong-311@users.noreply.github.com>
2023-12-04 22:00:10 +0900

    라우트를 설정하여 장바구니 컴포넌트를 렌더링
    라우트를 설정하여 장바구니 컴포넌트를 렌더링 하였습니다

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
-import React, { useRef, useState } from 'react';
+// App.js
+
+import React, { useState } from 'react';
 import BestBox from './component/BestBox';
 import Title from './component/Title';
 import BestList from './component/BestList';
@@ -6,13 +8,15 @@ import Header from './component/Header';
 import { initialBests } from './component/BestData';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Product from './component/Product';
+import ShoppingCart from './component/cart/ShoppingCart';
 
 function App() {
   const [bests, setBests] = useState(initialBests);
+  const [cartItems, setCartItems] = useState([]);
 
-  const onToggle = id => {
+  const onToggle = (id) => {
     setBests(
-      bests.map(best => (best.id === id ? { ...best, like: !best.like } : best))
+      bests.map((best) => (best.id === id ? { ...best, like: !best.like } : best))
     );
   };
 
@@ -20,15 +24,19 @@ function App() {
     <Router>
       <Header />
       <BestBox>
-        <Title name="BEST PRODUCT" />
         <Routes>
           <Route
             path="/"
-            element={<BestList bests={bests} onToggle={onToggle} />}
+            element={
+              <>
+                <Title name="BEST PRODUCT" />
+                <BestList bests={bests} onToggle={onToggle} />
+              </>
+            }
           />
-         <Route path="/:id" element={<Product />}></Route>
+          <Route path="/:id" element={<Product cartItems={cartItems} setCartItems={setCartItems} />} />
+          <Route path="/cart" element={<ShoppingCart cartItems={cartItems} setCartItems={setCartItems} />} />
         </Routes>
-        
       </BestBox>
     </Router>
   );
