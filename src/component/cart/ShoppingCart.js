441de5b828a7c3308dce0b1ddc93388dcadbf8fd
hong-311 <136770134+hong-311@users.noreply.github.com>
2023-12-06 21:09:37 +0900

    스타일 수정, 휴지통 아이콘 추가
    스타일 수정, 휴지통 아이콘 추가

--- src/component/cart/ShoppingCart.js
+++ src/component/cart/ShoppingCart.js
@@ -1,82 +1,97 @@
 import { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
+import { MdDelete } from "react-icons/md";
 
 const ShoppingCartBlock = styled.div`
   padding: 16px;
   margin: 16px;
   text-align: center;
-  background-color: #f5f5f5;
-
+  
   h2 {
-    color: #333;
-    margin-bottom: 16px;
-  }
-
-  ul {
-    list-style: none;
-    padding: 0;
-  }
-
-  li {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    margin-bottom: 8px;
-  }
-
-  img {
-    max-width: 60px;
-    height: auto;
-    margin-right: 8px;
-  }
-
-  p {
-    font-size: 16px;
-    color: #333;
-    margin: 0;
-  }
-
-  div.actions {
-    display: flex;
-    justify-content: flex-end;
-    margin-top: 16px;
-
-    button {
-      font-size: 18px;
-      padding: 8px 16px;
-      margin: 0 8px;
-      cursor: pointer;
-      border: none;
-      border-radius: 4px;
-      color: #333;
+      color: #f5f5f5;
+      margin-bottom: 16px;
     }
-
-    .checkout {
-      background-color: #e51a92;
-      color: #fff;
+    
+    ul {
+        list-style: none;
+        padding: 0;
     }
-
-    .continue-shopping {
-      background-color: #333;
-      color: #fff;
+    
+    li {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        margin-bottom: 8px;
+        border-bottom: 1px solid #ccc; 
+        padding-bottom: 8px;
+      }
+    
+    img {
+        width: 120px;
+        height: 120px;
+        margin-right: 8px;
     }
-  }
-`;
-
+    
+    p {
+        font-size: 16px;
+        #f5f5f5
+        margin: 0;
+    }
+    
+    div.actions {
+        display: flex;
+        justify-content: flex-end;
+        margin-top: 16px;
+        
+        
+        button {
+            font-size: 18px;
+            padding: 8px 16px;
+            margin: 0 8px;
+            cursor: pointer;
+            border: none;
+            border-radius: 4px;
+            color: #333;
+            
+        }
+        
+        .checkout {
+            background-color: #e51a92;
+            color: #fff;
+        }
+        
+        .continue-shopping {
+            background-color: #333;
+            color: #fff;
+        }
+    }
+    `;
+    
 const CountButtonBlock = styled.button`
-  font-size: 18px;
-  padding: 8px;
-  margin: 0 8px;
-  cursor: pointer;
-  border: none;
-  background-color: #333;
-  color: #fff;
-  border-radius: 4px;
+    font-size: 18px;
+    padding: 8px;
+    margin: 0 8px;
+    cursor: pointer;
+    border: none;
+    background-color: #333;
+    color: #fff;
+    border-radius: 4px;
+    `;
+const IconBlock = styled(MdDelete)`
+   font-size: 25px;
+  color: #333;
+  &:hover {
+    > path { 
+      color: #e51a92; 
+    }
+  }
 `;
+
 
 function ShoppingCart({ cartItems, handleCount, onCheckout }) {
   const [cart, setCart] = useState(cartItems);
+
   useEffect(() => {
     setCart(cartItems);
   }, [cartItems]);
@@ -86,7 +101,6 @@ function ShoppingCart({ cartItems, handleCount, onCheckout }) {
       setCart([]);
     };
   }, []);
-
 
   const handleRemove = (productId) => {
     const updatedCart = cart.filter(item => item.id !== productId);
@@ -111,14 +125,14 @@ function ShoppingCart({ cartItems, handleCount, onCheckout }) {
               </div>
 
               <div>
-                 <CountButtonBlock onClick={() => handleCount('minus')}>-</CountButtonBlock>
-                 <span style={{ color: 'black' }}>{item.quantity}</span>
-                 <CountButtonBlock onClick={() => handleCount('plus')}>+</CountButtonBlock>
+                <CountButtonBlock onClick={() => handleCount('minus')}>-</CountButtonBlock>
+                <span style={{ color: '#f5f5f5' }}>{item.quantity}</span>
+                <CountButtonBlock onClick={() => handleCount('plus')}>+</CountButtonBlock>
               </div>
 
-              <p>총 금액: ￦ {(item.price * item.quantity).toLocaleString()}</p>
+              <p>￦ {(item.price * item.quantity).toLocaleString()}</p>
 
-              <button style={{ color: 'black' }} onClick={() => handleRemove(item.id)}>삭제</button>
+              <IconBlock onClick={() => handleRemove(item.id)} />
             </li>
           ))}
         </ul>
