eec95750f120f59874b7c9d0fec1298d78ade95e
hong-311 <136770134+hong-311@users.noreply.github.com>
2024-01-21 21:52:32 +0900

    장바구니 알림 디자인 변경
    장바구니 알림 디자인 변경

--- src/component/cart/CartAlert.js
+++ src/component/cart/CartAlert.js
@@ -3,23 +3,32 @@ import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 
 const AlertContainer = styled.div`
-  background-color: #4CAF50;
-  color: #fff;
+  background-color: #FFE8F7;
+  color: #333;
   text-align: center;
-  padding: 10px;
+  font-weight: bold;
+  padding: 20px; 
   margin-top: 16px;
-  border-radius: 4px;
+  border-radius: 8px; 
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1); 
 `;
 
 const GoToCartButton = styled(Link)`
   background-color: #333;
   color: #fff;
-  padding: 8px 16px;
-  margin-top: 8px;
+  font-weight: normal;
+  padding: 12px 20px; 
+  margin-top: 12px; 
+  margin-left: 16px; 
   border: none;
-  border-radius: 4px;
+  border-radius: 8px;
   text-decoration: none;
   cursor: pointer;
+  transition: background-color 0.3s ease; 
+
+  &:hover {
+    background-color: #e51a92; 
+  }
 `;
 
 function CartAlert(props) {
